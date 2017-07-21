813181041f6ad999ea811ab8f3cd3fa75cee908b
Johny Moreira <jms5@cin.ufpe.br>
2017-07-20 22:37:36 -0300

    rename y-axis

--- visualization.py
+++ visualization.py
@@ -7,10 +7,9 @@ def plotScatter(categoryName, topProps, path):
 	plt.xticks(x_ticks, topProps.index.values, rotation='vertical')
 	plt.grid(linestyle='dotted', alpha=0.5)
 	colors = y
-	area = 150 * y*3
-	plt.scatter( x_ticks, y, c=colors, s=area, alpha=0.9, cmap=plt.cm.cool)
+	plt.scatter( x_ticks, y, c=colors, s=30, alpha=0.9, cmap=plt.cm.cool)
 	plt.title("Infobox properties frequency for %s" % categoryName)
-	plt.ylabel("Frequency")
+	plt.ylabel("Proportion")
 	plt.xlabel("Properties")
 	plt.savefig(path+categoryName+'.png', bbox_inches='tight')
 	plt.gcf().clear()
