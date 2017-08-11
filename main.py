2bead37b3fa3e1d0e9935ce1900c7359f3b13e80
Johny Moreira <jms5@cin.ufpe.br>
2017-08-10 21:27:35 -0300

    adding statistics from geo and datetime properties

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import my_csv as csv
 import statistics as stat
+import properties as prop
 import visualization as v
 import pandas as pd
 
 names = []
 categories = []
-columns = ["Count Articles", "Count Infoboxes", "Count Properties","Avg. Props", "std props", "median props", "var props", "cov props"]
+columns = ["Count Articles", "Count Infoboxes", "Count Infob. w/ Geoinfo", "Count infob. w/ Datetime","Count Total Properties", "Count Geo Props.", "Count datetime props", "Avg. Properties", "std props", "median props", "var props", "cov props"]
 path_to_plots = '../results/plots/'
+path_to_geoplots = '../results/plots/geo/'
+path_to_datetimeplots = '../results/plots/datetime/'
+countArticlesWithGeoProps = 0
+countGeoProps = 0
+countArticlesWithDateTimeProps = 0
+countDateTimeProps = 0
 
 # iterates over csv files and calculates infobox statistics
 for file in csv.readCSVDirectory("datasets/"):
@@ -17,21 +24,37 @@ for file in csv.readCSVDirectory("datasets/"):
 	# count elements
 	print("counting elements...")
 	articles, infoboxes, props = stat.countElements(category)
+	# geo properties
+	articlesWithGeoProps = prop.getGeoProps(category)
+	if(articlesWithGeoProps.size!=0):
+		countArticlesWithGeoProps, countGeoProps = prop.count(articlesWithGeoProps, articles, infoboxes, props)
+		topGeoProps = prop.topPropertiesByProportion(articlesWithGeoProps, 20, infoboxes)
+		v.plotScatter(categoryName, topGeoProps, path_to_geoplots, "Geo properties frequency for " + categoryName)
+	# temporal properties
+	articlesWithDateTimeProps = prop.getDateTimeProps(category)
+	if(articlesWithDateTimeProps.size!=0):
+		countArticlesWithDateTimeProps, countDateTimeProps = prop.count(articlesWithDateTimeProps, articles, infoboxes, props)
+		topDateTimeProps = prop.topPropertiesByProportion(articlesWithDateTimeProps, 20, infoboxes)
+		v.plotScatter(categoryName, topDateTimeProps, path_to_datetimeplots, "DateTime properties frequency for " + categoryName)
 	# get properties average
 	print("getting average infobox props...")
 	average, std, median, variance, covariance = stat.averageInfoboxProperties(category)
-	categories.append([articles, infoboxes, props, average, std, median, variance, covariance])
+	categories.append([articles, infoboxes, countArticlesWithGeoProps, countArticlesWithDateTimeProps, props, countGeoProps, countDateTimeProps, average, std, median, variance, covariance])
 	# get top 30 properties
 	print("getting top 30 properties...")
 	topProperties = stat.topPropertiesByProportion(category, 30, infoboxes)
 	# plot top properties
 	print("plotting scatter...")
-	v.plotScatter(categoryName, topProperties, path_to_plots)
+	v.plotScatter(categoryName, topProperties, path_to_plots, "Infobox properties frequency for " + categoryName)
 	print("------------------------------------------")
-	print("Category: %s" % file);
+	print("Category: %s" % categoryName)
 	print("Articles count: %s" % articles)
-	print("Infoboxes count: %s" % infoboxes)
+	print("Total Infoboxes count: %s" % infoboxes)
+	print("Infoboxes w/ Geo: %s" % countArticlesWithGeoProps)
+	print("Infoboxes w/ Datetime: %s" % countArticlesWithDateTimeProps)
 	print("Props count: %s" % props)
+	print("Geo props count: %s" % countGeoProps)
+	print("Geo date/time count: %s" % countDateTimeProps)
 	print("Avg. Props: %s" % average)
 	print("==========================================")
 
