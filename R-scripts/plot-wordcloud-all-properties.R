5e54618fcf8dfeb92fe162c5bcf0f9bc1db5d7bf
Johny Moreira <jms5@cin.ufpe.br>
2018-02-20 20:43:47 -0300

    add R script for top properties wordcloud

---
+++ R-scripts/plot-wordcloud-all-properties.R
@@ -0,0 +1,16 @@
+# set environment
+setwd("~/workspace/extraction-analytics/results")
+
+library(readr)
+library(RColorBrewer)
+library(wordcloud)
+library(ggplot2)
+
+top_props = read_csv("csv/top-props-frequency.csv")
+
+filename = 'plots/wordcloud_props'
+
+png(filename, width=6, height=6, units="in", res=600)
+cloud <- wordcloud(words = top_props$property, freq = top_props$frequency, 
+                   random.order = TRUE, colors=brewer.pal(8, "Dark2"))
+dev.off()
--- R-scripts/plot-wordcloud-templates.R
+++ R-scripts/plot-wordcloud-templates.R
@@ -11,10 +11,10 @@ files = list.files("csv/templates/")
 for (f in files)
 {
   df = read_csv(paste("csv/templates/", f,  sep=""))
-  filename = unlist(strsplit(f, "\\."))[1]
-  filename = paste('plots/template/', filename, '.png', sep="")
-  png(filename, width=12, height=12, units="in", res=600)
-  wordcloud(words = df$Template, freq = df$Count, 
+  filename = paste('plots/template/wordcloud/', unlist(strsplit(f, "\\."))[1], sep='')
+
+  png(filename, width=6, height=6, units="in", res=600)
+  cloud <- wordcloud(words = df$Template, freq = df$Count, 
                       random.order = TRUE, colors=brewer.pal(8, "Dark2"))
   dev.off()
 }
